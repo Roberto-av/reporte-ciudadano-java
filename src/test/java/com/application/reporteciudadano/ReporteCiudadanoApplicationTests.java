2b90869c27e2eeab1372fcdb3e324320560f708e
Roberto Aviles <avilesrobertoe@gmail.com>
2024-03-14 19:48:51 -0700

    first commit

---
+++ src/test/java/com/application/reporteciudadano/ReporteCiudadanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.reporteciudadano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteCiudadanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
