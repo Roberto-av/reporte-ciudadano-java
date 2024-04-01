fd132d4a41f2ba5125046d644a9183e17e609340
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-01 11:26:50 -0700

    Add security exception

---
+++ src/main/java/com/application/reporteciudadano/security/exceptions/UserRegistrationException.java
@@ -0,0 +1,8 @@
+package com.application.reporteciudadano.security.exceptions;
+
+public class UserRegistrationException extends RuntimeException {
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
+
