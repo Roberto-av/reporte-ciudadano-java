177e7ce74db58f972e11da9451c55bc2f5a3578e
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-01 11:25:10 -0700

    Add security config

---
+++ src/main/java/com/application/reporteciudadano/security/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.application.reporteciudadano.security.config;
+
+import com.application.reporteciudadano.entities.Role;
+import com.application.reporteciudadano.security.filter.JwtAuthenticationFilter;
+import com.application.reporteciudadano.security.services.UserDetailsServiceImp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final UserDetailsServiceImp userDetailsServiceImp;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(UserDetailsServiceImp userDetailsServiceImp, JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.userDetailsServiceImp = userDetailsServiceImp;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpS)throws Exception{
+        return httpS
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests->{
+                    requests.requestMatchers("/auth/**").permitAll();
+                    requests.requestMatchers("/api/report/save").hasAuthority(Role.ROLE_USER.toString());
+                    requests.requestMatchers("/api/report/", "/api/report/update/{id}", "/api/report/{id}").hasAuthority(Role.ROLE_ADMIN.toString());
+                    requests.anyRequest().authenticated();
+                })
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(cors->{
+                })
+                .logout(log-> log.logoutUrl("/auth/logout").permitAll())
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsServiceImp);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+}
---
+++ src/main/java/com/application/reporteciudadano/security/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.application.reporteciudadano.security.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
