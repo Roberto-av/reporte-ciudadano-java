cdbfd93316269d80ccbc9fe9f5847f7c204678a1
Roberto Aviles <avilesrobertoe@gmail.com>
2024-04-15 03:50:25 -0700

    Add UserResponseDTO

---
+++ src/main/java/com/application/reporteciudadano/controllers/dto/response/UserResponseDTO.java
@@ -0,0 +1,21 @@
+package com.application.reporteciudadano.controllers.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponseDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String username;
+    private String email;
+
+}
